df54112d11fa0f2c725f4ae246c438bcdd1273da
IREMY250 <ibyishakajeanremy0@gmail.com>
2025-11-19 13:13:55 +0200

    Adding permission

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,9 +4,8 @@ Script that fetches an employee's TODO list progress from a REST API
 and displays it in the specified format.
 """
 
-import sys
-
 import requests
+import sys
 
 
 def get_employee_todo_progress(employee_id: int) -> None:
